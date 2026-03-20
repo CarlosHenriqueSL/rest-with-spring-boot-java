48ab6d31eab73ce33838323fe0a62b7339c5180d
CarlosHenriqueSL <luiscarloslopes597@gmail.com>
2026-03-20 14:59:37 -0300

    First steps in Java with Spring Boot 3.4.0

---
+++ rest-with-spring-boot-java/src/main/java/br/com/CarlosHenriqueSL/model/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.CarlosHenriqueSL.model;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ rest-with-spring-boot-java/src/test/java/br/com/CarlosHenriqueSL/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.CarlosHenriqueSL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
