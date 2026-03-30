89f84f0570c002d5f3fb9a1056e0a7ce60b97a89
CarlosHenriqueSL <luiscarloslopes597@gmail.com>
2026-03-30 14:21:55 -0300

    Adding migrations support with FlyWay

--- rest-with-spring-boot-java/src/main/java/br/com/CarlosHenriqueSL/controllers/TestLogController.java
+++ rest-with-spring-boot-java/src/main/java/br/com/CarlosHenriqueSL/controllers/TestLogController.java
@@ -3,14 +3,16 @@ package br.com.CarlosHenriqueSL.controllers;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/test/v1")
 @RestController
 public class TestLogController {
 
     private Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
 
-    @GetMapping("/test")
+    @GetMapping
     public String testLog() {
         logger.debug("This is an DEBUG log");
         logger.info("This is an INFO log");
---
+++ rest-with-spring-boot-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+               (1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+               (2, 'Anchiano - Italy - 1500', 'Leonardo', 'Male', 'da Vinci'),
+               (4, 'Porbandar - India', 'Indira', 'Female', 'Ghandi'),
+               (6, 'Kentucky - US', 'Muhamamd', 'Male', 'Ali'),
+               (8, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+               (9, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
+
